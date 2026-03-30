b86c2ed6f15cd03ebafe4f7f7592746771c05ffc
Lindor <aminatacharlot.diop@epfedu.fr>
2026-03-30 12:31:56 +0000

    feat: ajout Category dans models/index.js

--- models/index.js
+++ models/index.js
@@ -1,4 +1,5 @@
 import sequelize from '../config/database.js';
 import User from './User.js';
+import Category from './Category.js';
 
-export { sequelize, User };
+export { sequelize, User, Category };
