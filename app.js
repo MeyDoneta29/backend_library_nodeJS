918a4d2ef0813dca5442e7cc8944f5b1fd8cab45
Lindor <aminatacharlot.diop@epfedu.fr>
2026-03-30 12:32:36 +0000

    feat: ajout route /api/categories dans app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.js';
+import categoriesRoutes from './routes/categories.js';
 
 dotenv.config();
 
@@ -9,8 +10,10 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static('uploads'));
 
 app.use('/api/auth', authRoutes);
+app.use('/api/categories', categoriesRoutes);
 
 export default app;
